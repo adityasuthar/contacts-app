27d1faa2195d239992afaa682f2be0b2cd859094
adityasuthar <sutharaditya2002@gmail.com>
2024-07-22 12:48:05 +0530

    initial commit

---
+++ contacts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
